b073a54b4680772aeb7ed7146efe44d218ab3e07
rala72 <code@rala.io>
2020-10-16 15:55:13 +0200

    add save commits to pouchdb

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import getCommitData from './util/db/getCommitData';
+import PouchDB from 'pouchdb';
 
-getCommitData().then(console.log);
+const db = new PouchDB('db');
+
+saveData()
+  .then(() => db.get('commits'))
+  .then(console.log);
+
+async function saveData(type = 'commits') {
+  const data = await getCommitData();
+  try {
+    const doc = await db.get(type);
+    return await db.put({
+      _id: type,
+      _ref: doc._ref,
+      [type]: data
+    });
+  } catch (err) {
+    if (err.status !== 404) return err;
+    return await db.put({
+      _id: type,
+      [type]: data
+    });
+  }
+}
