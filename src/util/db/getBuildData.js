609f2fae1d8af2fd53a684d2a3e9ed7de0f66670
rala72 <code@rala.io>
2020-10-19 13:48:05 +0200

    cleanup getBuildData

--- src/util/db/getBuildData.js
+++ src/util/db/getBuildData.js
@@ -1,49 +1,21 @@
 'use strict';
 
-import _ from 'lodash';
 import {graphQl, traversePages} from '../.';
-import getBounds from './getBounds';
 
-export default async function getBuildData(interval = 1) {
-  const {firstCommit, firstIssue} = await getBounds();
-
-  let next = Date.parse(firstIssue ?
-    firstIssue.createdAt : firstCommit.date);
-
-  const data = [
-    {
-      date: new Date(next),
-      stats: {
-        success: 0,
-        failed: 0,
-        pending: 0,
-        canceled: 0
-      }
-    }
-  ];
+export default async function getBuildData() {
+  const buildList = [];
 
   return traversePages(getBuildsPage, build => {
-    const createdAt = Date.parse(build.createdAt);
-
-    while (createdAt >= next) {
-      const dataPoint = {
-        date: new Date(next),
-        stats: _.defaults(
-          {
-            total:
-              (build.stats.success || 0) +
-              (build.stats.failed || 0) +
-              (build.stats.pending || 0) +
-              (build.stats.canceled || 0)
-          },
-          build.stats
-        )
-      };
-
-      data.push(dataPoint);
-      next += interval;
-    }
-  }).then(() => data);
+    buildList.push(Object.assign({}, build, {
+      _stats: {
+        total:
+          (build.stats.success || 0) +
+          (build.stats.failed || 0) +
+          (build.stats.pending || 0) +
+          (build.stats.canceled || 0)
+      }
+    }));
+  }).then(() => buildList);
 }
 
 const getBuildsPage = (page, perPage) => {
