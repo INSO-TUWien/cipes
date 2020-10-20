deafbf1cf86b6698dab3d07f32527460134837b5
rala72 <code@rala.io>
2020-10-20 11:49:55 +0200

    move db loading to functions

--- ui/index.js
+++ ui/index.js
@@ -1,3 +1,12 @@
-fetch('assets/db.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+init().then();
+
+
+async function init() {
+  const db = await fetchDb();
+  console.log(db);
+}
+
+function fetchDb() {
+  return fetch('assets/db.json')
+    .then(response => response.json());
+}
