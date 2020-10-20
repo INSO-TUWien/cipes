13d83b8d08eb8db9cfafdce1acb4f8028027218d
rala72 <code@rala.io>
2020-10-21 01:08:30 +0200

    reduce production file size

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,20 @@
 module.exports = {
+  productionSourceMap: false,
   publicPath: './',
   pages: {
     index: {
       entry: 'ui/main.js',
       chunks: ['chunk-vendors', 'chunk-common', 'index']
     }
+  },
+  chainWebpack: config => { // https://stackoverflow.com/a/61768334/2715720
+    config.optimization.minimizer('terser')
+      .tap((args) => {
+        args[0].terserOptions.output = {
+          ...args[0].terserOptions.output,
+          comments: false  // exclude all comments from output
+        }
+        return args
+      })
   }
 };
