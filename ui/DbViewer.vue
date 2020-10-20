962b4b1527a9b7c5a70decc63a1836f63354fbc3
rala72 <code@rala.io>
2020-10-20 21:40:24 +0200

    add tab content and props type checks

--- ui/DbViewer.vue
+++ ui/DbViewer.vue
@@ -1,19 +1,33 @@
 <template>
-  <b-tabs class="dbTabs h-100">
-    <b-tab title="Json">
-      <label for="db" class="sr-only">Database:</label>
-      <textarea id="db" disabled v-model="json" rows="5"></textarea>
+  <b-tabs class="dbTabs h-100" :justified="true">
+    <b-tab title="ALL">
+      <json-viewer :data="json"></json-viewer>
+    </b-tab>
+    <b-tab v-for="key in keys" :title="key" :key="key"
+           :disabled="!db[key] || db[key].length===0">
+      <json-viewer :data="db[key]"></json-viewer>
     </b-tab>
   </b-tabs>
 </template>
 
 <script>
+import JsonViewer from './JsonViewer';
+
 export default {
   name: 'DbViewer',
-  props: ['db'],
+  components: {JsonViewer},
+  props: {
+    'db': {
+      type: Object,
+      default: {}
+    }
+  },
   computed: {
     json: function () {
       return JSON.stringify(this.db, null, 2);
+    },
+    keys: function () {
+      return Object.keys(this.db);
     }
   }
 }
---
+++ ui/JsonViewer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label for="json" class="sr-only">Database:</label>
+    <textarea id="json" disabled v-model="json" rows="5"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JsonViewer',
+  props: {
+    'data': {
+      type: [String, Array, Object],
+      default: ''
+    }
+  },
+  computed: {
+    json: function () {
+      return typeof this.data === 'string' ? this.data :
+        JSON.stringify(this.data, null, 2);
+    }
+  }
+}
+</script>
+
+<style scoped>
+#json {
+  resize: none;
+  height: 100%;
+  width: 100%;
+}
+</style>
