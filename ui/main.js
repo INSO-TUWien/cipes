5c637c5de5639c301fcd92542faf71cdd53f0321
rala72 <code@rala.io>
2020-10-20 20:06:35 +0200

    remove vuex

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
 import App from './App';
 import 'bootstrap';
 import {library} from '@fortawesome/fontawesome-svg-core';
@@ -9,7 +8,6 @@ import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 library.add(faSearch);
 
 Vue.config.productionTip = false;
-Vue.use(Vuex);
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
