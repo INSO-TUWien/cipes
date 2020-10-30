98be0309b4b1da3c254f832cce144da1dcdb2520
rala72 <code@rala.io>
2020-10-30 10:18:04 +0100

    show valid query pattern

--- ui/Search.vue
+++ ui/Search.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="row">
     <div class="col">
-      <div class="input-group">
-        <label for="search" class="sr-only">Search:</label>
-        <input type="text" id="search" class="form-control"
-               autocomplete="off" v-model="searchText"
-               @keypress.enter="$emit('search',searchText, $event)">
+      <div class="form-group">
+        <div class="input-group">
+          <label for="search" class="sr-only">Search:</label>
+          <input type="text" id="search" class="form-control"
+                 autocomplete="off" v-model="searchText"
+                 @keypress.enter="$emit('search',searchText, $event)">
 
-        <div class="input-group-append">
-          <button type="button" class="btn btn-outline-primary"
-                  @click="$emit('search', searchText, $event)">
-            <font-awesome-icon icon="search"/>
-          </button>
+          <div class="input-group-append">
+            <button type="button" class="btn btn-outline-primary"
+                    @click="$emit('search', searchText, $event)">
+              <font-awesome-icon icon="search"/>
+            </button>
+          </div>
         </div>
+
+        <small class="form-text text-muted">
+          Valid query pattern: <code>-?\w+(\.\w+)*\??[~=&lt;&gt;]=?.*</code>
+        </small>
       </div>
     </div>
   </div>
