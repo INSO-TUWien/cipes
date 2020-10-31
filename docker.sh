b9042e80d17ee02891a2f7f4de9550e14b2c3dc9
rala72 <code@rala.io>
2020-10-31 23:56:34 +0100

    fix github alias missing

--- docker.sh
+++ docker.sh
@@ -4,4 +4,5 @@ export cipesDir=/home/cipes
 export cipesDist="$cipesDir/dist"
 
 shopt -s expand_aliases
+alias cipes-github="(cd $cipesDir && npm run prod-github)"
 alias cipes-gitlab="(cd $cipesDir && npm run prod-gitlab)"
