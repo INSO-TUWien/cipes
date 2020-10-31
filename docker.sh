65d89c7a3f24f08bca5e3789827f3a29bbb69eae
rala72 <code@rala.io>
2020-10-31 19:16:31 +0100

    update export variables script section

--- docker.sh
+++ docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-shopt -s expand_aliases
 
+shopt -s expand_aliases
 alias cipes-gitlab='(cd $cipesDir && npm run prod-gitlab)'
-export cipesDir="/home/cipes"
-export cipesDist="$cipesDir/dist"
+
+export cipesDir=/home/cipes
+export cipesDist=$cipesDir/dist
 
 arangod --server.endpoint tcp://0.0.0.0:8529 --database.password 'supersecret' &
 sleep 5
