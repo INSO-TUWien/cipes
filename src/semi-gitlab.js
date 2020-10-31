fe6d58a90b937576e1e3492a9a5b6d7a073e8b8d
rala72 <code@rala.io>
2020-10-31 02:28:29 +0100

    remove unnecessary -o in semi-gitlab

--- src/semi-gitlab.js
+++ src/semi-gitlab.js
@@ -1,5 +1,5 @@
 import {exec} from 'child_process';
 import {callback} from './semi';
 
-const prodServer = exec('npm run prod-server -o /builds');
+const prodServer = exec('npm run prod-server /builds');
 prodServer.stdout.on('data', data => callback(data));
