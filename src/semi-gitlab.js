935e9e798ad0ed43711ede195cd3e6485f756f03
rala72 <code@rala.io>
2020-10-31 11:22:09 +0100

    change update gitlab checkout folder

--- src/semi-gitlab.js
+++ src/semi-gitlab.js
@@ -1,5 +1,5 @@
 import {exec} from 'child_process';
 import {callback} from './semi';
 
-const prodServer = exec('npm run prod-server /builds');
+const prodServer = exec('npm run prod-server $CI_BUILDS_DIR');
 prodServer.stdout.on('data', data => callback(data));
