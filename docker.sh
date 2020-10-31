a3faa31b96f17a8a7800779b2f4896004a545eb9
rala72 <code@rala.io>
2020-10-31 21:57:53 +0100

    change variables to echo commands

--- docker.sh
+++ docker.sh
@@ -3,8 +3,10 @@
 shopt -s expand_aliases
 alias cipes-gitlab='(cd $cipesDir && npm run prod-gitlab)'
 
-export cipesDir=/home/cipes
-export cipesDist=$cipesDir/dist
+# variables not working in gitlab pipeline
+_cipesDir=/home/cipes
+alias cipesDir="echo $_cipesDir"
+alias cipesDist="echo $_cipesDir/dist"
 
 arangod --server.endpoint tcp://0.0.0.0:8529 --database.password 'supersecret' &
 sleep 5
