3171615e1a7e1b8b2e9db93206100a97dd298aff
rala72 <code@rala.io>
2020-10-31 23:05:49 +0100

    fix spelling mistakes in docker.sh files

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+export cipesDir=/home/cipes
+export cipesDist="$cipesDir/dist"
+
 shopt -s expand_aliases
 alias cipes-gitlab="(cd $cipesDir && npm run prod-gitlab)"
-
-expprt cipesDir=/home/cipes
-export cipesDist="$cipesDir/dist"
