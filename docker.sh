05a6d3c65f07f154a43191d97f530314333ccfa4
rala72 <code@rala.io>
2020-10-31 22:26:46 +0100

    remove empty lines

--- docker.sh
+++ docker.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 _cipesDir=/home/cipes
-
 shopt -s expand_aliases
 alias cipes-gitlab="(cd $_cipesDir && npm run prod-gitlab)"
-
 # variables not working in gitlab pipeline
 alias cipesDir="echo $_cipesDir"
 alias cipesDist="echo $_cipesDir/dist"
