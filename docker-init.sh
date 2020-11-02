2da9fb522c2e2a59635049c598f261e8f4bf4236
rala72 <code@rala.io>
2020-11-02 16:46:55 +0100

    add possibility to save cipesrcFile without configuration

--- docker-init.sh
+++ docker-init.sh
@@ -4,3 +4,15 @@ source /root/alias.sh
 
 arangod --server.endpoint tcp://0.0.0.0:8529 --database.password 'supersecret' &
 sleep 5
+
+export cipesrcFile="$CI_PROJECT_DIR"/.cipesrc
+if [ ! -f $cipesrcFile ] && [ -n "$CI_PROJECT_DIR" ] &&
+  [ -n "$CI_API_V4_URL" ] && [ -n "$CI_JOB_TOKEN" ]; then
+
+  echo "{" >"$cipesrcFile"
+  echo "  \"gitlab\": {" >>"$cipesrcFile"
+  echo "    \"url\": \"$CI_API_V4_URL\"," >>"$cipesrcFile"
+  echo "    \"token\": \"$CI_JOB_TOKEN\"" >>"$cipesrcFile"
+  echo "  }" >>"$cipesrcFile"
+  echo "}" >>"$cipesrcFile"
+fi
