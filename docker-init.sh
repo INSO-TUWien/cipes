eb5e3b021b1428eb41714b247e38ccb078a34257
rala72 <code@rala.io>
2020-11-02 18:55:05 +0100

    rename cipesrcFile to cipesrcPath

--- docker-init.sh
+++ docker-init.sh
@@ -5,15 +5,15 @@ source /root/alias.sh
 arangod --server.endpoint tcp://0.0.0.0:8529 --database.password 'supersecret' &
 sleep 5
 
-export cipesrcFile="$CI_PROJECT_DIR"/.cipesrc
-if [ ! -f $cipesrcFile ] && [ -n "$CI_PROJECT_DIR" ] &&
+export cipesrcPath="$CI_PROJECT_DIR"/.cipesrc
+if [ ! -f $cipesrcPath ] && [ -n "$CI_PROJECT_DIR" ] &&
   [ -n "$CI_SERVER_URL" ] && [ -n "$CI_JOB_TOKEN" ]; then
 
-  echo "{" >"$cipesrcFile"
-  echo "  \"gitlab\": {" >>"$cipesrcFile"
-  echo "    \"url\": \"$CI_SERVER_URL\"," >>"$cipesrcFile"
-  echo "    \"token\": \"$CI_JOB_TOKEN\"" >>"$cipesrcFile"
-  echo "  }" >>"$cipesrcFile"
-  echo "}" >>"$cipesrcFile"
-  echo "">>"$cipesrcFile"
+  echo "{" >"$cipesrcPath"
+  echo "  \"gitlab\": {" >>"$cipesrcPath"
+  echo "    \"url\": \"$CI_SERVER_URL\"," >>"$cipesrcPath"
+  echo "    \"token\": \"$CI_JOB_TOKEN\"" >>"$cipesrcPath"
+  echo "  }" >>"$cipesrcPath"
+  echo "}" >>"$cipesrcPath"
+  echo "">>"$cipesrcPath"
 fi
